88ad68965e1dbbe11ced3803f79dc03ff5616e0d
merve keser <mervekeser8888@gmail.com>
2023-09-17 18:17:12 +0300

    completed prime numbers

--- Java 101/Prime Numbers/src/Main.java
+++ Java 101/Prime Numbers/src/Main.java
@@ -1,17 +1,19 @@
+//1-100 prime numbers
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int number=100;
-        boolean isPrime;
+        boolean isPrime=true;
 
-        for(int i=2; i<=number; i++){
-            for(int k=2; k<i; k++){
-                if(i%k!=0 && i % (i/k)!=0){
-                    System.out.print(i+" ");
-                    break;
-                }
-            }
-        }
+       for(int number=2; number<=100; number++){
+           for(int i=2; i<number; i++){
+               if(number % i ==0){
+                   isPrime = false;
+                   break;
+               }
+               isPrime =true;
+           }
+           if(isPrime) System.out.print(number+" ");
+       }
     }
 }
